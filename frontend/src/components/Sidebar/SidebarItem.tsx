28f918b565dea09ceb6f7f73cad12846943f4bdb
German Viescas <germanviescas@gmail.com>
2021-12-11 19:41:47 -0300

    Add tooltip to sidebar menu items.

--- frontend/src/components/Sidebar/SidebarItem.tsx
+++ frontend/src/components/Sidebar/SidebarItem.tsx
@@ -1,5 +1,6 @@
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
+import Tooltip from '@mui/material/Tooltip'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import { useAuth } from 'lib/hooks/use-auth';
@@ -23,12 +24,14 @@ const SidebarItem = ({ icon, text, to, adminOnly }: Props): JSX.Element | null =
   }
 
   return (
-    <ListItem onClick={() => history.push(to)} selected={pathname === to}>
-      <ListItemIcon>
-        {icon}
-      </ListItemIcon>
-      <ListItemText primary={text} />
-    </ListItem>
+    <Tooltip title={text} arrow placement="right">
+      <ListItem onClick={() => history.push(to)} selected={pathname === to}>
+        <ListItemIcon>
+          {icon}
+        </ListItemIcon>
+        <ListItemText primary={text} />
+      </ListItem>
+    </Tooltip>
   )
 }
 
