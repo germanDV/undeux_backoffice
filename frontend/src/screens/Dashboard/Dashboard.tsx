2882b103fe713bca2ba02b1a1c25a5ea93bea7c6
German Viescas <germanviescas@gmail.com>
2022-02-19 20:27:14 -0300

    Show account balances in Dashboard.

--- frontend/src/screens/Dashboard/Dashboard.tsx
+++ frontend/src/screens/Dashboard/Dashboard.tsx
@@ -1,49 +1,24 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
-import Typography from '@mui/material/Typography'
 import { useTheme } from '@mui/material/styles'
+import { useAccounts } from 'lib/hooks/account'
 
 const Dashboard = () => {
   const theme = useTheme()
+  const acctData = useAccounts()
 
   return (
     <>
       <Box my={theme.spacing(3)}>
         <Paper elevation={3}>
           <Box py={theme.spacing(2)} px={theme.spacing(4)}>
-            <Typography paragraph>
-              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-              eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-              neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-              tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-              sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-              tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-              gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-              et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-              tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-              eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-              posuere sollicitudin aliquam ultrices sagittis orci a.
-            </Typography>
-          </Box>
-        </Paper>
-      </Box>
-      <Box my={theme.spacing(3)}>
-        <Paper elevation={3}>
-          <Box py={theme.spacing(2)} px={theme.spacing(4)}>
-            <Typography paragraph>
-              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-              eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-              neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-              tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-              sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-              tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-              gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-              et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-              tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-              eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-              posuere sollicitudin aliquam ultrices sagittis orci a.
-            </Typography>
+            {acctData.isLoading
+              ? 'Loading'
+              : acctData.isError
+                ? acctData.error.message
+                : acctData.data?.accounts.map(a => <p key={a.id}>{a.balance} ({a.currency})</p>)
+            }
           </Box>
         </Paper>
       </Box>
