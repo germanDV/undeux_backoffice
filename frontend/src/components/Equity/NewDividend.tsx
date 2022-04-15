9f14e83bcfbefedceb9dd9163c98972ae89f7e2b
German Viescas <germanviescas@gmail.com>
2022-04-15 15:48:17 -0300

    Ability to register dividends.

---
+++ frontend/src/components/Equity/NewDividend.tsx
@@ -0,0 +1,164 @@
+import { useEffect } from 'react'
+import { useFormik } from 'formik'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import Button from "@mui/material/Button"
+import Alert from "@mui/material/Alert"
+import { dividendSchema, DividendSubmission } from 'lib/schemas'
+import { usePayDividend } from 'lib/hooks/dividend'
+import { useShareholders } from 'lib/hooks/shareholder'
+import { nowUTC, dateToString, tr } from 'lib/helpers'
+import { Form, Separator } from 'ui/form.styles'
+import TextField from "@mui/material/TextField";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import esLocale from "date-fns/locale/es";
+import DatePicker from "@mui/lab/DatePicker";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+
+interface Props {
+  open: boolean
+  handleClose: () => void
+  handleSuccess: (msg: string) => void
+}
+
+const NewDividend = ({ open, handleClose, handleSuccess }: Props): JSX.Element => {
+  const theme = useTheme()
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'))
+  const mutation = usePayDividend()
+  const shareholdersData = useShareholders()
+
+  const formik = useFormik({
+    initialValues: {
+      date: nowUTC(),
+      amount: 0,
+      accountId: 0,
+      shareholderId: 0,
+    },
+    validationSchema: dividendSchema,
+    onSubmit: (data: DividendSubmission) => {
+      mutation.mutate({
+        ...data,
+        amount: Math.round(data.amount),
+        date: dateToString(data.date as unknown as Date),
+      })
+    },
+  })
+
+  if (formik.isSubmitting && (mutation.isError || mutation.isSuccess)) {
+    formik.setSubmitting(false)
+  }
+
+  const msg = mutation.data?.msg || ''
+  const { resetForm } = formik
+  useEffect(() => {
+      if (mutation.isSuccess) {
+        handleSuccess(msg)
+        resetForm()
+    }
+  }, [mutation.isSuccess, resetForm, msg, handleSuccess])
+
+  return (
+    <Dialog open={open} onClose={handleClose} fullScreen={fullScreen}>
+      <DialogTitle>Dividendo</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Registrar pago de dividendo.
+        </DialogContentText>
+        <Form onSubmit={formik.handleSubmit}>
+          <Separator />
+
+          <TextField
+            id="amount"
+            name="amount"
+            label="Importe (sin decimales)"
+            value={formik.values.amount === 0 ? '' : formik.values.amount}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.amount && !!formik.errors.amount}
+            helperText={formik.touched.amount && formik.errors.amount}
+          />
+          <Separator />
+
+          <LocalizationProvider dateAdapter={AdapterDateFns} locale={esLocale}>
+            <DatePicker
+              label="Fecha de pago"
+              value={formik.values.date}
+              onChange={(date: Date | null) => formik.setFieldValue('date', date)}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+          <Separator />
+
+          <FormControl fullWidth>
+            <InputLabel id="account">Cuenta</InputLabel>
+            <Select
+              labelId="account"
+              id="accountId"
+              name="accountId"
+              value={formik.values.accountId === 0 ? '' : formik.values.accountId}
+              label="Cuenta"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.accountId && !!formik.errors.accountId}
+            >
+              <MenuItem value={1}>ARS</MenuItem>
+              <MenuItem value={2}>USD</MenuItem>
+            </Select>
+            <FormHelperText className="Mui-error">
+              {formik.touched.accountId && formik.errors.accountId}
+            </FormHelperText>
+          </FormControl>
+          <Separator />
+
+
+          <FormControl fullWidth>
+            <InputLabel id="shareholder">Socio</InputLabel>
+            <Select
+              labelId="shareholder"
+              id="shareholderId"
+              name="shareholderId"
+              value={formik.values.shareholderId === 0 ? '' : formik.values.shareholderId}
+              label="Socio"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.shareholderId && !!formik.errors.shareholderId}
+            >
+              {shareholdersData.data?.shareholders?.length
+                ? shareholdersData.data.shareholders.map((s) => <MenuItem value={s.id} key={s.id}>{s.name}</MenuItem>)
+                : null}
+            </Select>
+            <FormHelperText className="Mui-error">
+              {formik.touched.shareholderId && formik.errors.shareholderId}
+            </FormHelperText>
+          </FormControl>
+          <Separator times={2} />
+
+          {mutation.isError && (
+            <div>
+              <Alert severity="error">
+                {tr((mutation.error as Error).message)}
+              </Alert>
+              <Separator />
+            </div>
+          )}
+        </Form>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancelar</Button>
+        <Button onClick={formik.submitForm}>Registrar</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default NewDividend
--- frontend/src/screens/Shareholders/Shareholders.tsx
+++ frontend/src/screens/Shareholders/Shareholders.tsx
@@ -9,22 +9,33 @@ import NewShareholder from 'components/ShareholderList/NewShareholder'
 import PageTitle from 'components/PageTitle/PageTitle'
 import EquityTable from 'components/Equity/EquityTable'
 import EquityChart from 'components/Equity/EquityChart'
+import NewDividend from 'components/Equity/NewDividend'
 import { PaperContainer } from 'ui/paper.styles'
 
 const Shareholders = (): JSX.Element => {
-  const [open, setOpen] = useState(false);
+  const [shareholderOpen, setShareholderOpen] = useState(false);
+  const [dividendOpen, setDividendOpen] = useState(false);
   const { enqueueSnackbar } = useSnackbar()
 
-  const handleClose = useCallback(() => {
-    setOpen(false)
-  }, [setOpen])
+  const handleShareholderClose = useCallback(() => {
+    setShareholderOpen(false)
+  }, [setShareholderOpen])
 
-  const handleSuccess = useCallback((id: number) => {
-    setOpen(false)
+  const handleShareholderSuccess = useCallback((id: number) => {
+    setShareholderOpen(false)
     enqueueSnackbar(`Socio ${id} creado exitosamente.`, {
       variant: 'success',
     })
-  }, [setOpen, enqueueSnackbar])
+  }, [setShareholderOpen, enqueueSnackbar])
+
+  const handleDividendClose = useCallback(() => {
+    setDividendOpen(false)
+  }, [setDividendOpen])
+
+  const handleDividendSuccess = useCallback((msg: string) => {
+    setDividendOpen(false)
+    enqueueSnackbar(msg, { variant: 'success' })
+  }, [setDividendOpen, enqueueSnackbar])
 
   return (
     <div>
@@ -42,21 +53,38 @@ const Shareholders = (): JSX.Element => {
       </PaperContainer>
 
       <ShareholderList />
+
       <Box sx={{ my: 8, display: 'flex', justifyContent: 'right' }}>
         <Fab
           color="primary"
           aria-label="add"
-          onClick={() => setOpen(true)}
+          onClick={() => setDividendOpen(true)}
+          variant="extended"
+          sx={{ mr: 1 }}
+        >
+          <AddIcon sx={{ mr: 1 }} />
+          Registrar Dividendo
+        </Fab>
+        <Fab
+          color="primary"
+          aria-label="add"
+          onClick={() => setShareholderOpen(true)}
           variant="extended"
         >
           <AddIcon sx={{ mr: 1 }} />
           Crear Socio
         </Fab>
       </Box>
+
+      <NewDividend
+        open={dividendOpen}
+        handleClose={handleDividendClose}
+        handleSuccess={handleDividendSuccess}
+      />
       <NewShareholder
-        open={open}
-        handleClose={handleClose}
-        handleSuccess={handleSuccess}
+        open={shareholderOpen}
+        handleClose={handleShareholderClose}
+        handleSuccess={handleShareholderSuccess}
       />
     </div>
   )
